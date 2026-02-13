d41c1af473723b497789be5c89e7552d3fb74ed1
jindidi0704 <jindidi0704@gmail.com>
2026-02-13 10:48:44 +0800

    Add "sys_info" in main.

--- cli_tool.cpp
+++ cli_tool.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <string>
-
+#include <fstream>
 using namespace std;
+
+void showCPUInfo() {
+    string line;
+    // 2. 開啟 Linux 的系統資訊檔 (在 WSL 或任何 Linux 都存在)
+    ifstream cpuFile("/proc/cpuinfo"); 
+
+    if (cpuFile.is_open()) {
+        while (getline(cpuFile, line)) {
+            // 3. 搜尋包含 "model name" 的那一行
+            // string::npos 是一個常數，代表「找不到」的意思
+            if (line.find("model name") != string::npos) {
+                cout << "[系統資訊] " << line << endl;
+                break; // 找到一行就夠了，不用把所有核心都印出來
+            }
+        }
+        cpuFile.close(); // 4. 記得關檔
+    } else {
+        cout << "[錯誤] 無法讀取系統資訊！" << endl;
+    }
+}
 
 void executeCommand(string cmd) {
     
     if (cmd == "help") {
-        cout << "[System] Commands: help, status, power_on, reboot, exit" << endl;
+        cout << "[System] Commands: help, status, sys_info, power_on, reboot, exit" << endl;
     }
     
     else if (cmd == "status") {
@@ -21,10 +41,15 @@ void executeCommand(string cmd) {
         cout << "[System] System is running." << endl;
     }
 
+    else if (cmd == "sys_info") {
+        showCPUInfo();
+    }
+
     else {
         cout << "[Error] Wrong Input." << endl;
     }
 }
+
 
 int main() {
 
@@ -38,13 +63,14 @@ int main() {
         cout << "Password:" ;
         
         getline(cin, password);
-
-        if (password == "1234"){
-            break;
-        }
-        else {
-            cout << "Wrong password!" << endl;
-            return 0;
+        
+        if (!password.empty()) {
+            if (password == "1234"){
+                break;
+            }
+            else {
+                cout << "Wrong password!" << endl;
+            }
         }
         
     }
